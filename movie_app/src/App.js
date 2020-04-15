5409b8636c45cb626c4951d3ef1051c63a56dcef
DOGUKKIM <59882993+Moderatio@users.noreply.github.com>
2020-04-15 18:04:17 +0900

    컴포넌트 작업

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
 
-function Hate(fav){
-return <h1>motherfucker{fav}</h1>
+function Movie({name,pic}){
+return <div>
+  <h1>I like {name}</h1>
+  <img src={pic} />
+</div>
 }
 
+const movieLike = [
+  {
+    name:"starwars",
+    image:"https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg"
+  },
+  {
+    name:"toystory",
+    image:"https://d2lzb5v10mb0lj.cloudfront.net/covers/600/30/3004625.jpg"
+  },
+  {
+    name:"harrypoter",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT6CJFJXYA7prvfjZh42a0uNA0fEOvQzS3XtW8QAfTRmWuzg2EB&usqp=CAU"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <h3>hello!!</h3>
-      <Hate  fav="satans" />
-    </div>
+      {movieLike.map(kindOf=> <Movie name={kindOf.name} pic={kindOf.image} />)}
+    </div>  
   );
 }
 
