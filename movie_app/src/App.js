67c40a4d8b9afcd9a1ca3e5a742a965a03d34a62
DOGUKKIM <59882993+Moderatio@users.noreply.github.com>
2020-04-16 17:30:29 +0900

    proptypes

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,30 +1,39 @@
 import React from 'react';
-function Movie({name,pic}){
+import propTypes from "prop-types";
+
+function Movie({name,pic,rating}){
   return <div>
     <h1>I like {name}</h1>
-    <img src={pic} />
+    <h4>{rating}/5.0</h4>
+    <img src={pic} alt={name} />
   </div>
   }
   
   const movieLike = [
     {
+      id:1,
       name:"starwars",
-      image:"https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg"
+      image:"https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg",
+      rating:5
     },
     {
+      id:2,
       name:"toystory",
-      image:"https://d2lzb5v10mb0lj.cloudfront.net/covers/600/30/3004625.jpg"
+      image:"https://d2lzb5v10mb0lj.cloudfront.net/covers/600/30/3004625.jpg",
+      rating:4.8
     },
     {
+      id:3,
       name:"harrypoter",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT6CJFJXYA7prvfjZh42a0uNA0fEOvQzS3XtW8QAfTRmWuzg2EB&usqp=CAU"
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT6CJFJXYA7prvfjZh42a0uNA0fEOvQzS3XtW8QAfTRmWuzg2EB&usqp=CAU",
+      rating:4
     }
   ]
   
   function App() {
     return (
       <div className="App">
-        {movieLike.map(movieList=> <Movie name={movieList.name} pic={movieList.image} />)}
+        {movieLike.map(movieList=> <Movie key={movieList.id} name={movieList.name} pic={movieList.image} rating={movieList.rating} />)}
       </div>  
     );
   }
