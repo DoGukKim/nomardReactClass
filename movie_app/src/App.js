505f913e51669611ec6b4521529e2035c07d3ac0
DOGUKKIM <59882993+Moderatio@users.noreply.github.com>
2020-04-17 13:56:05 +0900

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,41 +1,26 @@
 import React from 'react';
-import propTypes from "prop-types";
 
-function Movie({name,pic,rating}){
+class App extends React.Component{
+  state={
+    count:0
+  };
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }))
+  };
+  min = () => {
+    this.setState(current => ({ count: current.count - 1 }))
+  };
+  render(){
   return <div>
-    <h1>I like {name}</h1>
-    <h4>{rating}/5.0</h4>
-    <img src={pic} alt={name} />
+    <h1>The num is : {this.state.count}</h1>
+    <button onClick={this.add}>Add</button>
+    <button onClick={this.min}>Min</button>
   </div>
   }
   
-  const movieLike = [
-    {
-      id:1,
-      name:"starwars",
-      image:"https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg",
-      rating:5
-    },
-    {
-      id:2,
-      name:"toystory",
-      image:"https://d2lzb5v10mb0lj.cloudfront.net/covers/600/30/3004625.jpg",
-      rating:4.8
-    },
-    {
-      id:3,
-      name:"harrypoter",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT6CJFJXYA7prvfjZh42a0uNA0fEOvQzS3XtW8QAfTRmWuzg2EB&usqp=CAU",
-      rating:4
-    }
-  ]
-  
-  function App() {
-    return (
-      <div className="App">
-        {movieLike.map(movieList=> <Movie key={movieList.id} name={movieList.name} pic={movieList.image} rating={movieList.rating} />)}
-      </div>  
-    );
-  }
+}
+
+
+
 export default App;
 
